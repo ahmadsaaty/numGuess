2342a3c4ae28ad6174bbdffa0eb40ee9450827ab
Ahmad Saaty <ahmad.o.saaty@gmail.com>
2021-03-02 14:36:42 +0200

    third commit

--- script.js
+++ script.js
@@ -14,6 +14,17 @@ let guessCount = 1;
 
 guessSubmit.addEventListener('click', checkGuess)
 
+// Execute a function when the user releases a key on the keyboard
+guessField.addEventListener("keyup", function (event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        checkGuess();
+    }
+});
+
 function checkGuess() {
 
     let userGuess = Number(guessField.value);
